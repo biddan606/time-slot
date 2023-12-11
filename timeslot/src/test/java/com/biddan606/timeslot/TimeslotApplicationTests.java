8cfc32af70c0e71c2cdba77b55ee37c9a64511c7
biddan606 <biddan606@gmail.com>
2023-12-11 20:49:24 +0900

    init: Spring Boot 프로젝트 생성

---
+++ timeslot/src/test/java/com/biddan606/timeslot/TimeslotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biddan606.timeslot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeslotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
